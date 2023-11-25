770d8804ee3300e6fb6db7b43d905cb3dcf08196
GabyAM <gdgabyam@gmail.com>
2023-11-25 12:39:14 -0300

    adds function to map the weather response
    since i don't need all of the data from the api, with this function i'm returning only the data i'll use.

---
+++ src/getWeather.js
@@ -0,0 +1,37 @@
+import { fetchWeather } from "./fetchWeather.js";
+
+function getMappedWeather(weatherJson) {
+	function mapWeatherMoment(weather) {
+		return {
+			tempC: weather.temp_c,
+			tempF: weather.temp_f,
+			condition: weather.condition.text,
+			windKph: weather.wind_kph,
+			humidity: weather.humidity,
+		};
+	}
+
+	const mappedWeather = {
+		location: {
+			name: weatherJson.location.name,
+			country: weatherJson.location.country,
+		},
+		current: {
+			lastUpdated: weatherJson.current.last_updated,
+			...mapWeatherMoment(weatherJson.current),
+		},
+		forecast: {},
+	};
+	mappedWeather.forecast = weatherJson.forecast.forecastday.map((day) => {
+		const hours = day.hour.map((hour) => {
+			return {
+				time: hour.time.slice(11),
+				chanceOfRain: hour.chance_of_rain,
+				...mapWeatherMoment(hour),
+			};
+		});
+
+		return { hours, condition: day.day.condition.text };
+	});
+	return mappedWeather;
+}
