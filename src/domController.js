dca0bf48bc65755bdf95a841a54d34930ab5900f
GabyAM <gdgabyam@gmail.com>
2023-11-27 09:00:46 -0300

    adds icons to the day buttons
    adds a styleDayButtons that check if the condition name is someway in the icons object, and if it is uses the google material icon corresponding to it. If it's not uses the default icon that comes with the API.

--- src/domController.js
+++ src/domController.js
@@ -1,6 +1,48 @@
 import { weatherController } from "./weatherController.js";
 
 export const domController = (function () {
+	const icons = {
+		// might change to fontawesome icons later
+		sunny: "sunny",
+		rain: "rainy",
+		clear: "clear_night",
+		cloudy: "filter_drama",
+		"partly cloudy": "partly_cloudy_day",
+		fog: "foggy",
+		mist: "mist",
+		sleet: "weather_hail",
+		snow: "cloudy_snowing",
+		thunder: "thunderstorm",
+	};
+
+	function styleDayButton(button, condition) {
+		function isWordInObject(word) {
+			return Object.keys(icons).some((key) => word.includes(key));
+		}
+		let icon;
+		if (isWordInObject(condition.name.toLowerCase())) {
+			icon = document.createElement("span");
+			icon.className = "material-symbols-outlined";
+			const conditionName = condition.name.toLowerCase();
+
+			if (conditionName.includes("rain")) {
+				icon.textContent = icons.rain;
+			} else if (conditionName.includes("sleet")) {
+				icon.textContent = icons.sleet;
+			} else if (conditionName.includes("snow")) {
+				icon.textContent = icons.snow;
+			} else if (conditionName.includes("thunder")) {
+				icon.textContent = icons.thunder;
+			} else {
+				icon.textContent = icons[conditionName];
+			}
+		} else {
+			icon = document.createElement("img");
+			icon.src = condition.icon;
+		}
+		button.appendChild(icon);
+	}
+
 	function loadButtons() {
 		const $dayButtons = document.querySelectorAll(".day-button");
 		$dayButtons.forEach(async (button, index) => {
@@ -12,6 +54,7 @@ export const domController = (function () {
 			});
 
 			const condition = await weatherController.getDayCondition(index);
+			styleDayButton(button, condition);
 		});
 	}
 
