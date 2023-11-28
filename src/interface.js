6ee2e09926643bb33cd4dd40f41020b443926754
GabyAM <gdgabyam@gmail.com>
2023-11-28 15:02:31 -0300

    adds weatherInterface module
    this module will work as an intermediary between the weatherController module and the domControllerModule, so the dom actions are not directly modifying the data.

---
+++ src/interface.js
@@ -0,0 +1,26 @@
+import { weatherController } from "./weatherController.js";
+
+export const weatherInterface = (function () {
+	function changeHour(newHour) {
+		weatherController.setHour(newHour);
+	}
+	function changeDay(dayIndex) {
+		weatherController.setDay(dayIndex);
+	}
+
+	function getHourWeather(newHour) {
+		changeHour(newHour);
+		return weatherController.getHourWeather();
+	}
+
+	function getDayWeather(dayIndex) {
+		changeDay(dayIndex);
+		return weatherController.getDayWeather();
+	}
+
+	function isToday() {
+		return weatherController.getCurrentDayIndex() === 0;
+	}
+
+	return { getHourWeather, isToday };
+})();
